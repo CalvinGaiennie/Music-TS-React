1fb0892bed811a438d76c70b6abc27b9e0c6b0fb
CalvinGaiennie <calvingaiennie@gmail.com>
2025-07-15 23:31:53 -0500

    small note

--- src/pages/MetronomePage.tsx
+++ src/pages/MetronomePage.tsx
@@ -4,7 +4,9 @@ function MetronomePage() {
       <h1 className="mb-4">Metronome</h1>
       <div>Metronome</div>
       <p>stick control workout</p>
-      <p>tomo's finger exercise (try it before posting it here)</p>
+      <p>
+        tomo's finger exercise (try it more extensively before posting here)
+      </p>
       <p>scales. maybe integrate this with the fretboard simulator</p>
       <p>Maybe do some strumming patterns and exercises</p>
     </div>

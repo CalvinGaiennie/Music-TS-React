02bee646265b250157a159c9f8f06298ea183a00
CalvinGaiennie <calvingaiennie@gmail.com>
2025-07-23 21:34:55 -0500

    commit message

--- src/pages/UserContributionPage.tsx
+++ src/pages/UserContributionPage.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import LessonContributionForm from "../components/LessonContributionForm";
+// import { useState } from "react";
+// import LessonContributionForm from "../components/LessonContributionForm";
 import TrackContributionForm from "../components/TrackContributionForm";
 
 function UserContributionPage() {
-  const [contributionType, setContributionType] = useState("lesson");
+  //   const [contributionType, setContributionType] = useState("lesson");
 
   return (
     <div className="container d-flex flex-column align-items-center">
       <h1 className="mb-4">Contribute</h1>
-      <div className="mb-4">
+      {/* <div className="mb-4">
         <p>What would you like to contribute?</p>
         <select
           className="form-select"
@@ -17,12 +17,13 @@ function UserContributionPage() {
         >
           <option value="lesson">Lesson</option>
           <option value="track">Track</option>
-          {/* <option>Metronome Workout</option> */}
+          <option>Metronome Workout</option>
         </select>
-      </div>
+      </div> */}
       <div>
-        {contributionType === "lesson" && <LessonContributionForm />}
-        {contributionType === "track" && <TrackContributionForm />}
+        {/* {contributionType === "lesson" && <LessonContributionForm />}
+        {contributionType === "track" && <TrackContributionForm />} */}
+        <TrackContributionForm />
       </div>
     </div>
   );
